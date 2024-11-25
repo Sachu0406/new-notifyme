3a0e989bf6c9b730d59d2435b3d70fda6b9473a6
adedatharam <adedatharam@gmail.com>
2024-11-25 05:13:21 +0000

    state dependency removed

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -8,12 +8,12 @@ import { CarouselItem } from "../Shared/staticData";
 import NotificationCards from "./AllNotifications";
 
 const Home = () => {
-  const [searchParams] = useSearchParams();
+ // const [searchParams] = useSearchParams();
   const [pageRefresh, setPageRefresh] = useState<number>(Math.random());
   const [carouselItems, setCarouselItems] = useState<CarouselItem[]>([]);
-  const state = searchParams.get("state") || 0;
-  const [showDialogue] = useState<boolean>(state ? false : true);
-  const { stateList, getAllStates } = useAllDataStore();
+  //const state = searchParams.get("state") || 0;
+  //const [showDialogue] = useState<boolean>(state ? false : true);
+  //const { stateList, getAllStates } = useAllDataStore();
   // const [currentState, setCurrentState] = useState<iState>({
   //   districts: [],
   //   _id: "",
@@ -21,39 +21,39 @@ const Home = () => {
   //   stateName: "",
   //   stateId: 0,
   // });
-  const getStates = useCallback(() => {
-    if (stateList?.length === 0 && +state !== 0) {
-      getAllStates();
-    }
-  }, [stateList?.length === 0]);
+  // const getStates = useCallback(() => {
+  //   if (stateList?.length === 0 && +state !== 0) {
+  //     getAllStates();
+  //   }
+  // }, [stateList?.length === 0]);
 
-  useEffect(() => {
-    if (stateList?.length > 0 && +state !== 0) {
-      // const matchedState: any = stateList?.find(
-      //   (obj: any) => obj.stateId === +state
-      // );
-      //setCurrentState(matchedState);
-    }
-    getStates();
-  }, [stateList, state]);
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://dummyjson.com/products?limit=10');
-      const data = await response.json();
-      const items = data.products.map((item: any) => ({
-        id: item.id,
-        title: item.title,
-        image: item.images[0],
-      }));
-      setCarouselItems(items);
-      setPageRefresh(Math.random());
-    } catch (error) {
-      console.error('Error fetching carousel data:', error);
-    }
-  };
-  useEffect(() => {
-    if (state) fetchData();
-  }, []);
+  // useEffect(() => {
+  //   if (stateList?.length > 0 && +state !== 0) {
+  //     const matchedState: any = stateList?.find(
+  //       (obj: any) => obj.stateId === +state
+  //     );
+  //     setCurrentState(matchedState);
+  //   }
+  //   getStates();
+  // }, [stateList, state]);
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch('https://dummyjson.com/products?limit=10');
+  //     const data = await response.json();
+  //     const items = data.products.map((item: any) => ({
+  //       id: item.id,
+  //       title: item.title,
+  //       image: item.images[0],
+  //     }));
+  //     setCarouselItems(items);
+  //     setPageRefresh(Math.random());
+  //   } catch (error) {
+  //     console.error('Error fetching carousel data:', error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   if (state) fetchData();
+  // }, []);
 
   return (
     <div key={pageRefresh}>
@@ -66,7 +66,7 @@ const Home = () => {
         carouselItems={[]}
       />}
       <NotificationCards/>
-      {showDialogue && <SelectStateModal />}
+      {/* {showDialogue && <SelectStateModal />} */}
     </div>
   );
 };
