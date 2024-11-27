81c96b766f6335c8e4facf88e95dc13cdaeac05a
sachinnunavath123 <sachinnunavath123@gmail.com>
2024-11-27 04:40:39 +0000

    add new job form added and get all notification api integrated

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SwiperSection from "../Shared/CommonSwiper";
 import { gridData } from "../Shared/staticData";
 import NotificationCards from "./AllNotifications";
 import SwiperCards from "../Shared/SwiperCards";
+import useAllDataStore from "../APIStore/Store";
 
 const Home = () => {
+  const {getAllNotificationList} = useAllDataStore();
+  const [allData, setAllData] = useState<any>([]);
+  //const [newNotifyData, setNewNotifyData] = useState<any>();
+  async function fetchNotifications(){
+    try{
+      const res:any = await getAllNotificationList();
+      setAllData(res)
+    }catch(err){
+      console.log(err)
+    }
+  };
+  useEffect (()=>{
+    fetchNotifications();
+  },[]); 
+  
   const [pageRefresh] = useState<number>(Math.random());
   return (
     <div key={pageRefresh}>
@@ -16,7 +32,7 @@ const Home = () => {
       {<SwiperSection
         title="All Notifications"
         moreInfoText="Scroll Down"
-        carouselItems={[]}
+        carouselItems={allData}
       />}
       <NotificationCards
         gridData={gridData}
