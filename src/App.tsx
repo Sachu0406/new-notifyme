f0fb00b8490969205fd8c02ba6f3ecc427a8271f
Nunavath Sachu <sachinnunavath@gmail.com>
2024-11-30 11:23:14 +0530

    build changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,28 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout";
 import { AuthProvider } from "./AuthContext";
-import { useLocation } from "react-router-dom";
+//import { useLocation } from "react-router-dom";
 import Loader from "./Components/Shared/Loader";
 import { useLoader } from "./Components/Shared/UseLoader";
 import { ThemeProvider } from "./ThemeContext";
 
-const NotificationForm = lazy(() => import("./Components/Pages/NotificationForm"));
+const NotificationForm = lazy(
+  () => import("./Components/Pages/NotificationForm")
+);
 const ErrorPage = lazy(() => import("./Components/Shared/Error404"));
 const HomePage = lazy(() => import("./Components/Pages/Home"));
-const ManageNotificationsGrid = lazy(() => import("./Components/Pages/ManageNotificationsGrid"));
+const ManageNotificationsGrid = lazy(
+  () => import("./Components/Pages/ManageNotificationsGrid")
+);
 
-function useShouldShowLayout() {
-  const location = useLocation();
-  const pathsWithoutLayout = ["/Login", "/Register"];
-  return !pathsWithoutLayout.includes(location.pathname);
-}
+// function useShouldShowLayout() {
+//   const location = useLocation();
+//   const pathsWithoutLayout = ["/Login", "/Register"];
+//   return !pathsWithoutLayout.includes(location.pathname);
+// }
 
 function App() {
-  const shouldShowLayout = useShouldShowLayout();
+  //const shouldShowLayout = useShouldShowLayout();
   const loading = useLoader();
 
   const publicRoutes = (
@@ -28,8 +32,14 @@ function App() {
         <Route path="*" Component={ErrorPage} />
         <Route path="/" Component={HomePage} />
         <Route path="/addNewNotification" Component={NotificationForm} />
-        <Route path="/editNotification/:notificationId" Component={NotificationForm} />
-        <Route path="/manageNotificationsData" Component={ManageNotificationsGrid} />
+        {/* <Route
+          path="/editNotification/:notificationId"
+          Component={NotificationForm}
+        /> */}
+        <Route
+          path="/manageNotificationsData"
+          Component={ManageNotificationsGrid}
+        />
       </Routes>
     </Suspense>
   );
@@ -37,11 +47,9 @@ function App() {
     <AuthProvider>
       <ThemeProvider>
         {loading && <Loader />}
-        {shouldShowLayout ? <Layout>
-          <div className="container">
-            {publicRoutes}
-          </div>
-        </Layout> : publicRoutes}
+        <Layout>
+          <div className="container">{publicRoutes}</div>
+        </Layout>
       </ThemeProvider>
     </AuthProvider>
   );
