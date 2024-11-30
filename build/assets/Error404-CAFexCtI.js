5f6ac608e221661746de79ff9d6140827e0b2d87
Nunavath Sachu <sachinnunavath@gmail.com>
2024-11-30 08:40:30 +0530

    git ignore updated

--- build/assets/Error404-CAFexCtI.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./index-xeIlzTUP.js";const o=()=>r.jsxs("div",{className:"container text-center",children:["Error occured! Click here to  ",r.jsx("a",{href:"/",children:"Go Home"})]});export{o as default};
-//# sourceMappingURL=Error404-CAFexCtI.js.map
