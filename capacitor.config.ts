c9e44174c58b8a05f98133db72724f1c93038df5
Nunavath Sachu <sachinnunavath@gmail.com>
2024-12-07 14:27:41 +0530

    mobileapp created
    using ref

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "in.co.notifyme",
+  appName: "notifyme",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
+
+//refer the below link for build and process
+//https://chatgpt.com/share/67511770-0ed8-8005-8f1b-d52858c92a1b
